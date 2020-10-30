a061d2ccdef7218be8d5e792415680341bffb627
soresenja <reza11.6a@gmail.com>
2020-10-30 22:58:51 +0700

    belajar php

---
+++ FahrezaMaulanaWisnuWardana_BBondowoso/tipe_data.php
@@ -0,0 +1,16 @@
+<?php 
+
+// float
+$float = 10.2;
+// char
+$jenkel = "L";
+// string
+$nama = "Reza";
+// int
+$umur = 20;
+// boolean
+$makan = true;
+
+
+
+ ?>
---
+++ FahrezaMaulanaWisnuWardana_BBondowoso/variable.php
@@ -0,0 +1,5 @@
+<?php 
+	$barang = "Minyak kelapa";
+	$harganya = 150000;
+	echo "Saya beli ".$barang." Harganya : ".$harganya;
+ ?>
