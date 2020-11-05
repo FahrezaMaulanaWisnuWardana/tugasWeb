a05ab65642137ad4594a606021504d395dee3ca9
AzizRahmawan <71802858+AzizRahmawan@users.noreply.github.com>
2020-11-05 08:50:32 +0700

    Update login.php

--- AbdulAzizRahmawan_E41191468_BWS(B)/Praktikum Blok/Login Bootstrap/login.php
+++ AbdulAzizRahmawan_E41191468_BWS(B)/Praktikum Blok/Login Bootstrap/login.php
@@ -1,17 +1,12 @@
 <?php
     error_reporting (E_ALL ^ E_NOTICE);
     $login = [
-      [
-        'username' => "aziz",
-        'password' => "admin"
-      ],[
-        'username' => "rahmawan",
-        'password' => "123"
-      ]
-      ];
+      'username' => "aziz",
+      'password' => "admin"
+    ];
         if($_POST['user'] == $login['username']){
           if($_POST['pass'] == $login['password']){
-            header("Location: index.php"); 
+            header('Location: index.php');
           }else{
             echo "Password salah";
           }
